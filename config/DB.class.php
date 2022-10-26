26df7f21f02c27e054ec3ebc3d0aeb39a2a8707e
GabrielPaixao <gabriel.passion@gmail.com>
2022-10-26 14:06:15 -0300

    Update DB.class.php

--- config/DB.class.php
+++ config/DB.class.php
@@ -4,7 +4,7 @@
 		private static $conn;
 		static function getConn(){
 			if(is_null(self::$conn)){
-				self::$conn = new PDO("mysql:host=localhost;dbname=gmpxcom_cativou","gmpxcom_cativou","1q2w3e!@#");
+				self::$conn = new PDO("mysql:host=localhost;dbname=???","???","???");
 				self::$conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 			}
 			return self::$conn;			
